88c93d5a77744aa49af2302cdf038459bd3cf878
User <User@USER-PC>
2022-02-09 16:42:07 +0800

    Add project files.

---
+++ HyperlinkChecker/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Text.RegularExpressions;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string html = File.ReadAllText(@"C:\C# Stuff\HyperlinkChecker\test.txt");
+
+        Console.WriteLine("HTML Content: \n" + html + "\n");
+
+        Console.WriteLine("Contains Hyperlink: \n" + RemoveAnchor(html));
+    }
+
+    const string pattern = @"<(| +)(?:a\b[^>]*>|/a>)";
+
+    public static bool RemoveAnchor(string source)
+    {
+        return Regex.IsMatch(source, pattern, RegexOptions.IgnoreCase);
+    }
+}
+
